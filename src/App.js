5734989d62deba0fd2588101ffc54d5757ea3778
Viktor J. Helgason <Viktor.J.Helgason@landsbankinn.is>
2020-07-14 16:09:46 +0000

    Small charts table

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import axios from 'axios';
 
 import { Chart } from "react-google-charts";
 
+import SmallChart from './SmallChart';
+
 export default class App extends React.Component {
   state = {
-    chartData: []
+    chartData: [],
+    oilData: [],
   }
 
   componentDidMount() {
@@ -31,6 +34,26 @@ export default class App extends React.Component {
           console.log(chartData)
           this.setState({ chartData });
       })
+      
+    axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/CLQ20.NYM?region=US&lang=en-US&includePrePost=false&interval=1d&range=1y&corsDomain=finance.yahoo.com&.tsrc=finance`)
+    .then(res => {
+      console.log(res.data.chart.result[0])
+      const timestamps = res.data.chart.result[0].timestamp;
+      const values = res.data.chart.result[0].indicators.quote[0].close;
+      console.log(timestamps);
+      console.log(values);
+
+      let oilData = [["Timestamp", "Crude Oil"]]
+      timestamps.forEach(
+        (element, index) =>  {
+          const date = new Date(element*1000); // Unix time in ms
+          const prettyDate = date.toDateString()
+          oilData.push([prettyDate,values[index]])
+        }
+        );
+        console.log(oilData)
+        this.setState({ oilData });
+    })
   }
 
   render() {
@@ -45,14 +68,27 @@ export default class App extends React.Component {
     return (
       <>
         <h1>Yahoo Finance</h1>
-        <Chart
-        chartType="LineChart"
-        data={this.state.chartData}
-        width="100%"
-        height="500px"
-        legendToggle
-        options={options}
-        />
+        <SmallChart symbol="ARION.IC"/>
+        <SmallChart symbol="BRIM.IC"/>
+        <SmallChart symbol="EIK.IC"/>
+        <SmallChart symbol="EIM.IC"/>
+        <SmallChart symbol="FESTI.IC"/>
+        <SmallChart symbol="HAGA.IC"/>
+        <SmallChart symbol="HEIMA.IC"/>
+        <SmallChart symbol="ICEAIR.IC"/>
+        <SmallChart symbol="ICESEA.IC"/>
+        <SmallChart symbol="KVIKA.IC"/>
+        <SmallChart symbol="MAREL.IC"/>
+        <SmallChart symbol="ORIGO.IC"/>
+        <SmallChart symbol="REGINN.IC"/>
+        <SmallChart symbol="REITIR.IC"/>
+        <SmallChart symbol="SIMINN.IC"/>
+        <SmallChart symbol="SJOVA.IC"/>
+        <SmallChart symbol="SKEL.IC"/>
+        <SmallChart symbol="SYN.IC"/>
+        <SmallChart symbol="TM.IC"/>
+        <SmallChart symbol="VIS.IC"/>
+
       </>
     )
   }
